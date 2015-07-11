fe0b8dcc5a25d63f683aa60db413820f2e4eb074
Wako <dulo.rodrigo@gmail.com>
2015-07-11 13:13:27 -0500

    Debug

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ function texto() {
 	var a=new Date();
 	var hora = a.getHours();
 	
-	if(hora <= 11){
+	if(hora<=6){
+		document.getElementById("text").innerHTML="おやすみ";
+	}
+	else if(hora <= 11){
 		document.getElementById("text").innerHTML="おはよう";
 	}
-	else if(hora<=21){
+	else if(hora<=20){
 		document.getElementById("text").innerHTML="こんにちは";
 	}
 	else{
