039de02d8a03536a566091f2f392c91d9321122b
Wako <dulo.rodrigo@gmail.com>
2015-07-13 23:52:18 -0500

    Array

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function texto() {
 	var a=new Date();
 	var hora = a.getHours();
+	var msj = ["おはよう", "こんにちは", "おやすみ"];
 	
 	if(hora<=6){
-		document.getElementById("text").innerHTML="おやすみ";
+		document.getElementById("text").innerHTML=msj[2];
 	}
 	else if(hora <= 11){
-		document.getElementById("text").innerHTML="おはよう";
+		document.getElementById("text").innerHTML=msj[0];
 	}
 	else if(hora<=20){
-		document.getElementById("text").innerHTML="こんにちは";
+		document.getElementById("text").innerHTML=msj[1];
 	}
 	else{
-		document.getElementById("text").innerHTML="おやすみ";
+		document.getElementById("text").innerHTML=msj[2];
 	}
 }
 
